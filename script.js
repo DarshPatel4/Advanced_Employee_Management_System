eb9021150f650ca2f39ea056aefcc87c3b0772bd
Darsh Patel <jayjalaram25104@gmail.com>
2024-04-28 15:34:32 +0530

    change in UI full

--- script.js
+++ script.js
@@ -31,3 +31,33 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.messages-section').classList.remove('show');
   });
 });
+
+
+const sidemenu=document.querySelector('aside')
+const menuBtn=document.querySelector('#menu_bar')
+const closeBtn=document.querySelector('#close_btn')
+
+const themeToggler=document.querySelector('.theme-toggler')
+menuBtn.addEventListener('click',()=>{
+  sidemenu.style.display="block"
+})
+closeBtn.addEventListener('click',()=>{
+  sidemenu.style.display="none"
+})
+themeToggler.addEventListener('click',()=>{
+  document.body.classList.toggle('dark-theme-variables')
+  themeToggler.querySelector('span:nth-child(i)').classList.toggle('active')
+  themeToggler.querySelector('span:nth-child(i)').classList.toggle('active')
+
+})
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var aboutUsLink = document.getElementById("about_us_link");
+  var aboutUsContent = document.querySelector(".about-us-content");
+
+  aboutUsLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    aboutUsContent.style.display = (aboutUsContent.style.display === "none") ? "block" : "none";
+  });
+});
